8158e01855a84fece89ce45b92b2bb977a4750c0
rlarudals <gim181004@gmail.com>
2020-04-18 15:39:16 +0900

    commit

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import AppShell from "./AppBar";
+import TextScreen from "./TextScreen";
+import FileScreen from "./FileScreen";
+import WordCloudScreen from "./WordCloudScreen";
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <AppShell>
+          <div>
+            <Route exact path="/" component={TextScreen} />
+            <Route exact path="/file" component={FileScreen} />
+            <Route exact path="/wc" component={WordCloudScreen} />
+          </div>
+        </AppShell>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/WordCloudScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+
+class WordCloudScreen extends React.Component {
+  render() {
+    return (
+      <Card>
+        <CardContent>This page is WordCloudScreen</CardContent>
+      </Card>
+    );
+  }
+}
+
+export default WordCloudScreen;
